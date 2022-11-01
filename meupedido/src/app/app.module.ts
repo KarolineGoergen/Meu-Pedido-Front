eb8f11041ac7b20ee39b87e10c6ed37d0e0c68fa
KarolineGoergen <goergenkarol@gmail.com>
2022-11-01 19:46:43 -0300

    home

--- meupedido/src/app/app.module.ts
+++ meupedido/src/app/app.module.ts
@@ -27,12 +27,14 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 //Componentes
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
