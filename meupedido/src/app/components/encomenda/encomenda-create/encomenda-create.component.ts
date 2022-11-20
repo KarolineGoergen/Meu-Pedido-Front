eaf2f55e6c08aa651e05ad99a88794d2a5cc7526
KarolineGoergen <goergenkarol@gmail.com>
2022-11-19 23:15:52 -0300

    select de itens selecionados

--- meupedido/src/app/components/encomenda/encomenda-create/encomenda-create.component.ts
+++ meupedido/src/app/components/encomenda/encomenda-create/encomenda-create.component.ts
@@ -52,7 +52,23 @@ export class EncomendaCreateComponent implements OnInit {
     this.findAllCliente();
     this.findAllProduto();
   }
-  
+
+  qtd = "";
+  tarefa = "";
+  produtos = []
+  quantidade = []
+
+  addTarefa(){
+
+    this.produtos.push(this.tarefa)
+
+  }
+
+  addQtdTarefa(){
+    console.log(this.quantidade)
+    this.quantidade.push(this.qtd)
+  }
+
   addProduto(idProduto: number): void{
 
     //if(this.encomenda.itens.length == 0) {
